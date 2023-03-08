d33857cbc00c31c9c9398497fe2c298775153153
Nathan <nwbond87@gmail.com>
2023-03-08 10:00:32 -0600

    Start, button, First Chapter

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const listen = event => {
+    const p = document.createElement('p');
+    p.innerText = "Ah! Welcome. I hope you are ready. Ahead of you are two paths. The first, stretching to the right, disppears a distance into the fog. The second, heading left, leads into the setting sun."
+    document.querySelector('#begin').appendChild(p)
+    }
+
+
+
+
