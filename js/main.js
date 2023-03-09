1d4a8faf12aa03e8bc154897a25ee701211973a2
Nathan <nwbond87@gmail.com>
2023-03-09 11:31:03 -0600

    Added Code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+console.log("Main-test");
+
+
 const listen = event => {
     const p = document.createElement('p');
     p.innerText = "You find yourself just inside a massive archway formed of ivy. In front of you stands an enormous wall of thorns and greenery, blocking your path forward. To your left is a straight path leading toward the setting sun. To your right there is a winding road that disappears into a thick fog."
